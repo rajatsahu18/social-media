acc06785de97f270d416406f58a9b76aafc0df6a
rajatsahu18 <sahurajat78@gmail.com>
2023-09-27 16:05:26 +0530

    feat: created pages and redux components

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,7 +1,34 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Home, Login, Profile, Register, ResetPassword } from "./pages";
+
+function Layout() {
+  const { user } = useSelector((state) => state.user);
+  console.log("user", user);
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="login" state={{ from: location }} replace />
+  );
+}
+
 function App() {
+  const { theme } = useSelector((state) => state.theme);
+
   return (
-    <div className="w-full min-h-[100vh]">
-      <p className="text-blue">Hello Rajat</p>
+    <div data-theme={theme} className="w-full min-h-[100vh]">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id?" element={<Profile />} />
+        </Route>
+
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </div>
   );
 }
