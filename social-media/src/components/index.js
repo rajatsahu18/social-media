168b066aeae39354b2ebd455192278efbedc2ce4
rajatsahu18 <sahurajat78@gmail.com>
2023-09-28 15:39:35 +0530

    feat: created suggested friends friend requests page

--- social-media/src/components/index.js
+++ social-media/src/components/index.js
@@ -3,5 +3,6 @@ import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import TopBar from "./TopBar";
 import ProfileCard from "./ProfileCard";
+import FriendsCard from "./FriendsCard";
 
-export { TextInput, Loading, CustomButton, TopBar, ProfileCard }
+export { TextInput, Loading, CustomButton, TopBar, ProfileCard, FriendsCard }
