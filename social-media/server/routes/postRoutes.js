548b256a5f35ad3087ef5d01b350eb0ed9eecc25
rajatsahu18 <sahurajat78@gmail.com>
2023-10-06 23:58:19 +0530

    feat: created apis for getting posts users posts comments like the post like comments and comment on post

--- social-media/server/routes/postRoutes.js
+++ social-media/server/routes/postRoutes.js
@@ -1,13 +1,37 @@
-import express from 'express'
-import userAuth from '../middleware/authMiddleware.js'
-import { createPost, getPosts } from '../controllers/postController.js'
+import express from "express";
+import userAuth from "../middleware/authMiddleware.js";
+import {
+  createPost,
+  getPost,
+  getPosts,
+  getUserPost,
+  getComments,
+  likePost,
+  likePostComment,
+  commentPost,
+  replyPostComment,
+} from "../controllers/postController.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // create post
-router.post("/create-post", userAuth, createPost)
+router.post("/create-post", userAuth, createPost);
 
 // get posts
-router.post("/", userAuth, getPosts)
+router.post("/", userAuth, getPosts);
+router.post("/:id", userAuth, getPost);
+
+router.post("/get-user-post/:id", userAuth, getUserPost);
+
+// get comments
+
+router.get("/comments/:postId", getComments);
+
+// like comment on posts
+router.post("/like/:id", userAuth, likePost);
+router.post("/like-comment/:id/:rid", userAuth, likePostComment);
+router.post("/comment/:id", userAuth, commentPost);
+router.post("/reply-comment/:id", userAuth, replyPostComment);
+
 
 export default router;
